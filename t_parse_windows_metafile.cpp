2150f64969667fa81a7ac2f69a27c73e92f4c89b
ysouyno <ysouyno@163.com>
2021-12-23 13:17:24 +0800

    initial project files added

---
+++ t_parse_windows_metafile.cpp
@@ -0,0 +1,9 @@
+// t_parse_windows_metafile.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
